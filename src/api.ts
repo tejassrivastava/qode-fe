61f308075496d3c61cdef7110628d1ffb97a87f0
Tejas Srivastava <tejas.srivastava@hotmail.com>
2023-09-11 15:23:45 +0530

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://qode-be.onrender.com', // Replace with your backend API URL
+  baseURL: 'http://localhost:3000', // Replace with your backend API URL
 });
 
 export default instance;
