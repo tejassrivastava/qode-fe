3f92aea0ca95364b116d04d957cfae116e83a828
tejassrivastava <tejas.srivsatava@hotmail.com>
2023-09-11 12:05:52 +0530

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Box, Heading, Center, SimpleGrid } from "@chakra-ui/react";
 
 import UploadForm from "./components/UploadForm.tsx";
 import PhotoList from "./components/PhotoList.tsx";
+import axios from "./api.ts";
 
 const App = ():JSX.Element => {
   type Photo = {
@@ -10,6 +11,17 @@ const App = ():JSX.Element => {
     url: string;
     comment: string;
   };
+    // Defining a function for fetching the photos data from the backend
+    const fetchPhotos = async () => {
+      const data = await axios.get("/getPhoto");
+      if (data) {
+        setPhotos(data.data);
+      }
+    };
+  
+    useEffect(() => {
+      fetchPhotos();
+    }, []);
   const [photos, setPhotos] = useState<Photo[]>([]);
   return (
     <Box bgColor="gray.800" w="100vw" minH="100vh" p={8}>
@@ -25,7 +37,7 @@ const App = ():JSX.Element => {
         </Heading>
       </Center>
       <SimpleGrid minChildWidth="400px" spacing="1px" paddingTop="20px">
-        <PhotoList photos={photos} photoupdate={setPhotos} />
+        <PhotoList photos={photos} />
       </SimpleGrid> </>}
     </Box>
   );
--- src/components/PhotoList.tsx
+++ src/components/PhotoList.tsx
@@ -3,21 +3,11 @@ import PhotoCard from "./PhotoCard";
 import axios from "../api";
 
 // Defining a component for displaying all the photos and their comments
-const PhotoList = (props: { photos: any; photoupdate: any; }) :JSX.Element => {
+const PhotoList = (props: { photos: any; }) :JSX.Element => {
   // Defining a state variable for storing the photos data
-  const { photos, photoupdate } = props;
+  const { photos } = props;
 
-  // Defining a function for fetching the photos data from the backend
-  const fetchPhotos = async () => {
-    const data = await axios.get("/getPhoto");
-    if (data) {
-      photoupdate(data.data);
-    }
-  };
 
-  useEffect(() => {
-    fetchPhotos();
-  }, []);
 
   return (
     <>
